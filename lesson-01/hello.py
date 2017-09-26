ef793965c9b81cf4bcc0ada77ae22ee65a4e1a77
Евгений Чопенко <evg.chopenko@outlook.com>
2017-09-26 20:26:56 +0300

    Initial commit

---
+++ lesson-01/hello.py
@@ -0,0 +1,54 @@
+print ("hello,world")
+"""
+fafaga
+"""
+# pep 8
+
+is_student = True # boll с большой Т и тлько с большой
+name = input ("What is you name" )
+print("Hello",name,'!')
+
+
+
+# скаляр типы данных
+# bool True or False
+#int,float
+# str
+
+i1 = 666
+i2 = 0x59
+i3 = 0b01
+i4 = 0o7
+f1= 1.23
+f2= 12e-3
+
+
+s3 = r'Raw string'
+s4= u'лох' # utf
+
+# struct струтурированого типа
+t2 = (True, 1,1.2,"string", (1,2,3))
+t2[1] # ===1
+# списки
+lst1=[[],[],[],3,False]
+
+lst1[3] ## === 3
+# множество set
+set1 = {1,2,3}
+set2 = set() # пустое множество !!!элементы во множестве не повторяються!!!
+
+
+# словари
+d = {
+    'id':1,
+    'name': 'вася',
+
+
+}
+
+
+# спец типы
+None
+
+
+
